b121f30e8a46bdc83720741a8b5441e3c881414a
umair rajah <yourajah1993@gmail.com>
2021-03-01 19:31:20 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,12 @@
+
+// var main =document.getElementById('mainheader')
+// function detail(){
+//     main.style.display='none'
+//     var b = document.createElement('p')
+//     var a =document.createTextNode('price:1500')
+//   b.appendChild(a)
+
+
+
+
+// }
